1877f45eadbb609eb9a038794c5b0a3995c7be80
DorianDelavault <74056088+DorianDelavault@users.noreply.github.com>
2023-02-01 18:25:37 +0100

    push01-02)23
    darkmode

--- js/action.js
+++ js/action.js
@@ -1,28 +1,24 @@
-$(document).ready(function() {
+const btn = document.querySelector(".btn-toggle");
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 
-    $(window).scroll(function(e){
-        var scrollTop = $(window).scrollTop();
-        var docHeight = $(document).height();
-        var winHeight = $(window).height();
-        var scrollPercent = (scrollTop) / (docHeight - winHeight);
-        var scrollPercentRounded = Math.round(scrollPercent*100);
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme == "dark") {
+  document.body.classList.toggle("dark-theme");
+} else if (currentTheme == "light") {
+  document.body.classList.toggle("light-theme");
+}
 
-        $('#scrollPercentLabel>span').html(scrollPercentRounded);
-        repositionLabel();
-    });
-
-    $(window).resize(function(){
-        repositionLabel();
-    });
-
-    function repositionLabel() {
-        $('#scrollPercentLabel').css({
-            position:'fixed',
-            left: ($(window).width() - $('#scrollPercentLabel').outerWidth()) / 2,
-            top: (($(window).height() - $('#scrollPercentLabel').outerHeight()) / 2) - $('#scrollPercentLabel').height()
-        });
-    }
-
-    repositionLabel();
-
+btn.addEventListener("click", function () {
+  if (prefersDarkScheme.matches) {
+    document.body.classList.toggle("light-theme");
+    var theme = document.body.classList.contains("light-theme")
+      ? "light"
+      : "dark";
+  } else {
+    document.body.classList.toggle("dark-theme");
+    var theme = document.body.classList.contains("dark-theme")
+      ? "dark"
+      : "light";
+  }
+  localStorage.setItem("theme", theme);
 });
